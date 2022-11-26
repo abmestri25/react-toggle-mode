3b4e7bd51ed33e55cff44610f5975742357f1220
Abhishek <abmestri25@gmail.com>
2022-11-26 18:00:16 +0530

    Feat : Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Switch from "./components/Switch";
 
 function App() {
+  const [theme, setTheme] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? " container light" : "container dark"}>
+      <Switch theme={theme} setTheme={setTheme} />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Switch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Switch = ({theme,setTheme}) => {
+  return (
+    <div className="switch-container">
+    <h4>Dark Mode</h4>
+    <span className="switch square">
+      <input
+        onChange={() => setTheme(!theme)}
+        type="checkbox"
+        id="switch-square"
+      />
+      <label htmlFor="switch-square"></label>
+    </span>
+    <h4>Light Mode</h4>
+  </div>
+  )
+}
+
+export default Switch
